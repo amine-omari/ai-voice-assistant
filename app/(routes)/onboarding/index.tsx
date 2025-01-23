34c2cf77e5355ff74d2af88d5c4addcae3782b66
amine omari <omari1amineco@gmail.com>
2025-01-23 18:51:09 +0300

    feat: import OnBoardingScreen to Onboarding page

--- app/(routes)/onboarding/index.tsx
+++ app/(routes)/onboarding/index.tsx
@@ -1,10 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
+import OnBoardingScreen from "@/screens/onboarding.screens";
 
 export default function Onboarding() {
-  return (
-    <View>
-      <Text>Onboarding</Text>
-    </View>
-  );
+  return <OnBoardingScreen />;
 }
