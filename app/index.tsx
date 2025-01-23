62841eaa131049409bffd1c58e47d243c3aedfbd
amine omari <omari1amineco@gmail.com>
2025-01-23 18:39:11 +0300

    feat: add redirect

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from "react-native";
 import React, { useEffect, useState } from "react";
+import { Redirect } from "expo-router";
 
 export default function index() {
-//   const [isOnboarding, setIsOnboarding] = useState(true);
-//   useEffect(() => {}, []);
+  //   const [isOnboarding, setIsOnboarding] = useState(true);
+  //   useEffect(() => {}, []);
 
-  return <View></View>;
+  return <Redirect href="/(routes)/onboarding" />;
 }
+ 
