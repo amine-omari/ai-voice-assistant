114fd3d4201c444fd65aec4fba599de66d98bd74
amine omari <omari1amineco@gmail.com>
2025-01-23 19:12:29 +0300

    feat: add gradient styling

--- screens/onboarding.screens.tsx
+++ screens/onboarding.screens.tsx
@@ -1,11 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function OnBoardingScreen() {
   return (
-    <View>
+    <LinearGradient colors={["#250152", "#000000"]}>
       <Text>OnBoardingScreen</Text>
-    </View>
+    </LinearGradient>
   );
 }
 
