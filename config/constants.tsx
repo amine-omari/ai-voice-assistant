31f4f574e4069c36ed0ef1e61a05a93d0edf4105
amine omari <omari1amineco@gmail.com>
2025-01-24 13:54:17 +0300

    feat: add constants file

---
+++ config/constants.tsx
@@ -0,0 +1,16 @@
+import OnBoarding1 from "@/assets/svgs/onboarding1";
+
+export const onBoardingData = [
+  {
+    id: 1,
+    title: "",
+    subtitle: "",
+    image: <OnBoarding1 />,
+  },
+  {
+    id: 2,
+    title: "",
+    subtitle: "",
+    image: <OnBoarding1 />,
+  },
+];
