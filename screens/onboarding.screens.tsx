caa47321ed066c08058357822fdcf4a67e3114eb
amine omari <omari1amineco@gmail.com>
2025-01-24 16:19:27 +0300

    feat: add props

--- screens/onboarding.screens.tsx
+++ screens/onboarding.screens.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { ScrollView, StatusBar, StyleSheet, View } from "react-native";
+import {
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { onBoardingData } from "@/config/constants";
 import { onBoardingDataType } from "@/config/global";
 
 export default function OnBoardingScreen() {
+  const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {};
+
   return (
     <LinearGradient
       colors={["#250152", "#000000"]}
@@ -13,7 +22,12 @@ export default function OnBoardingScreen() {
       style={styles.container}
     >
       <StatusBar barStyle="light-content" />
-      <ScrollView horizontal>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onScroll={handleScroll}
+      >
         {onBoardingData.map((item: onBoardingDataType, index: number) => (
           <View key={index}></View>
         ))}
