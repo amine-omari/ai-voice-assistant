2cfdb2c3f1b63446340e3e42cfbf9929db053eef
amine omari <omari1amineco@gmail.com>
2025-01-24 17:58:42 +0300

    feat: add 2 more svgs to onboarding

--- config/constants.tsx
+++ config/constants.tsx
@@ -1,5 +1,7 @@
 import OnBoarding1 from "@/assets/svgs/onboarding1";
 import { onBoardingDataType } from "./global";
+import OnBoarding2 from "@/assets/svgs/onboarding2";
+import OnBoarding3 from "@/assets/svgs/onboarding3";
 
 export const onBoardingData: onBoardingDataType[] = [
   {
@@ -14,13 +16,13 @@ export const onBoardingData: onBoardingDataType[] = [
     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum?",
     subtitle:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum?",
-    image: <OnBoarding1 />,
+    image: <OnBoarding2 />,
   },
   {
     id: 3,
     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum?",
     subtitle:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum?",
-    image: <OnBoarding1 />,
+    image: <OnBoarding3 />,
   },
 ];
