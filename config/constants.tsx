cf1246a8a063a0e4f79c8a8aeaf96154abac51b5
amine omari <omari1amineco@gmail.com>
2025-01-24 14:44:35 +0300

    fix: types

--- config/constants.tsx
+++ config/constants.tsx
@@ -1,6 +1,7 @@
 import OnBoarding1 from "@/assets/svgs/onboarding1";
+import { onBoardingDataData } from "./global";
 
-export const onBoardingData = [
+export const onBoardingData: onBoardingDataData[] = [
   {
     id: 1,
     title: "",
@@ -9,6 +10,12 @@ export const onBoardingData = [
   },
   {
     id: 2,
+    title: "",
+    subtitle: "",
+    image: <OnBoarding1 />,
+  },
+  {
+    id: 3,
     title: "",
     subtitle: "",
     image: <OnBoarding1 />,
