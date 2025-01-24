9d0a0ae8ba6747e04005971a79bb3958876b045f
amine omari <omari1amineco@gmail.com>
2025-01-24 17:57:51 +0300

    feat: add font to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SegoeUI: require("../assets/fonts/Segoe-UI.ttf"),
   });
 
   useEffect(() => {
