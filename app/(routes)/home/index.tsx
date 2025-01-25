2e5f10c1e71e4225b56699557141c00760a5a0cc
amine omari <omari1amineco@gmail.com>
2025-01-25 12:51:20 +0300

    feat: import HomeScreen to home page

--- app/(routes)/home/index.tsx
+++ app/(routes)/home/index.tsx
@@ -1,10 +1,6 @@
-import { View, Text } from "react-native";
 import React from "react";
+import HomeScreen from "@/screens/home.screen";
 
 export default function Home() {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
+  return <HomeScreen />;
 }
