70b34d37ebd101eeebe1f93687129c17c049ffc0
amine omari <omari1amineco@gmail.com>
2025-01-25 11:14:42 +0300

    feat: add gradient colors

--- screens/home.screen.tsx
+++ screens/home.screen.tsx
@@ -4,7 +4,7 @@ import { LinearGradient } from "expo-linear-gradient";
 
 export default function HomeScreen() {
   return (
-    <LinearGradient colors={[]}>
+    <LinearGradient colors={["#250152", "#000"]}>
       <Text>HomeScreen</Text>
     </LinearGradient>
   );
