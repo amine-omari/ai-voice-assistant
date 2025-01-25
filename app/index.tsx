f5437308b741a72ddee96109f80924c3379d4454
amine omari <omari1amineco@gmail.com>
2025-01-25 11:04:26 +0300

    feat: add checkOnboarding function to useEffect

--- app/index.tsx
+++ app/index.tsx
@@ -5,5 +5,15 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 export default function index() {
   const [isOnboarding, setIsOnboarding] = useState(true);
 
+  useEffect(() => {
+    const checkOnboarding = async () => {
+      const isOnboarding = await AsyncStorage.getItem("onboarding");
+      if (isOnboarding) {
+        setIsOnboarding(false);
+      }
+    };
+    checkOnboarding();
+  }, []);
+
   return <Redirect href="/(routes)/onboarding" />;
 }
