8e3456f7786a93a40497df1a07d1d8b4da53c2f0
amine omari <omari1amineco@gmail.com>
2025-01-25 13:04:47 +0300

    feat: add purple blur to home screen

--- screens/home.screen.tsx
+++ screens/home.screen.tsx
@@ -1,7 +1,7 @@
 import { Image, StatusBar, StyleSheet } from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
-import { scale } from "react-native-size-matters";
+import { scale, verticalScale } from "react-native-size-matters";
 
 export default function HomeScreen() {
   return (
@@ -21,6 +21,16 @@ export default function HomeScreen() {
           right: scale(-15),
           top: 0,
           width: scale(240),
+        }}
+      />
+
+      <Image
+        source={require("@/assets/main/purple-blur.png")}
+        style={{
+          position: "absolute",
+          left: scale(-15),
+          bottom: verticalScale(100),
+          width: scale(210),
         }}
       />
     </LinearGradient>
