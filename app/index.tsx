728094fd6151ede57c386a29d4751bcef1812a85
amine omari <omari1amineco@gmail.com>
2025-01-25 11:01:54 +0300

    feat: add useState

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Redirect } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function index() {
-  //   const [isOnboarding, setIsOnboarding] = useState(true);
-  //   useEffect(() => {}, []);
+  const [isOnboarding, setIsOnboarding] = useState(true);
 
   return <Redirect href="/(routes)/onboarding" />;
 }
