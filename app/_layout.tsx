c59c11c350e2c469a9c12e3cde4bdabfa49f9ea4
amine omari <omari1amineco@gmail.com>
2025-01-25 11:10:39 +0300

    fix: warning

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -35,8 +35,8 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
-        <Stack.Screen name="/(routes)/onboarding" />
-        <Stack.Screen name="/(routes)/home" />
+        <Stack.Screen name="(routes)/onboarding/index" />
+        <Stack.Screen name="(routes)/home/index" />
       </Stack>
     </ThemeProvider>
   );
