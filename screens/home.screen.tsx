8f97a2aefcfcca3863b6e112f36018687e9cad8a
amine omari <omari1amineco@gmail.com>
2025-01-25 11:22:06 +0300

    feat: add start ad end of gradient

--- screens/home.screen.tsx
+++ screens/home.screen.tsx
@@ -4,7 +4,11 @@ import { LinearGradient } from "expo-linear-gradient";
 
 export default function HomeScreen() {
   return (
-    <LinearGradient colors={["#250152", "#000"]}>
+    <LinearGradient
+      colors={["#250152", "#000"]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+    >
       <Text>HomeScreen</Text>
     </LinearGradient>
   );
