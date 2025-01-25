54d85c5893447db279024e658a97c5bf047e0ee2
amine omari <omari1amineco@gmail.com>
2025-01-25 12:22:45 +0300

    feat: add container style

--- screens/home.screen.tsx
+++ screens/home.screen.tsx
@@ -8,10 +8,18 @@ export default function HomeScreen() {
       colors={["#250152", "#000"]}
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 1 }}
+      style={styles.container}
     >
       <Text>HomeScreen</Text>
     </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#131313",
+  },
+});
